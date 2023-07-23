778b63c3b71b936e668e9942f68a7372b6a883ae
Heaxeus <redgames13@gmail.com>
2023-07-23 14:11:58 -0700

    Initial Commit

---
+++ SDDRandomizer/Plugin.cs
@@ -0,0 +1,14 @@
+﻿using BepInEx;
+using BepInEx.NET.Common;
+
+namespace SDDRandomizer;
+
+[BepInPlugin("heaxeus.plugins.sddrandomizer", "SDD Randomizer", "1.0.0")]
+public class Plugin : BasePlugin
+{
+    public override void Load()
+    {
+        // Plugin startup logic
+        Log.LogInfo("SDDRandomizer is Loaded!");
+    }
+}
